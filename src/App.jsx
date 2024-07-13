a7ba7bcd846d2219c6270e41222c4b52a586f390
yosef-tutian <yoseftutian@gmail.com>
2024-07-13 20:42:41 +0300

    DeleteTask
    Option to delete a task from a project

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,14 @@ function App() {
     });
   }
 
-  function handleDeleteTask() {}
+  function handleDeleteTask(id) {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        tasks: prevState.tasks.filter((task) => task.id !== id),
+      };
+    });
+  }
   function handleSelectProject(id) {
     setProjectsState((prevState) => {
       return {
@@ -108,6 +115,7 @@ function App() {
         onStartAddProject={handleStartAddProject}
         projects={projectsState.projects}
         onSelectProject={handleSelectProject}
+        selectedProjectId={projectsState.selectedProjectId}
       />
       {content}
     </main>
